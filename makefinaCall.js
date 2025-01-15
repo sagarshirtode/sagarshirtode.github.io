3b92d43b415b8a0d6c83eb1f7e3d2d41bedcc521
sagarshirtode112 <sagarshirtode112@gmail.com>
2025-01-15 20:45:20 +0530

    add cyrb53 incryption

--- makefinaCall.js
+++ makefinaCall.js
@@ -114,10 +114,26 @@ async function generateFingerprint() {
   const encryptedFingerprint = await sha256(combined);
   return encryptedFingerprint;
 }
-
+function cyrb53(str, seed = 0) {
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1 =
+    Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^
+    Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 =
+    Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^
+    Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+}
 // Example Usage
 generateFingerprint().then((fingerprint) => {
   console.log("Device Fingerprint:", fingerprint);
+  var finalOutrPut = cyrb53(fingerprint);
   const element = document.getElementById("fingerprint");
-  element.innerHTML = fingerprint;
+  element.innerHTML = finalOutrPut;
 });
